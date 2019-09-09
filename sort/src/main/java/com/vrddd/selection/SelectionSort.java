b28eeec5c99a542883153a5270db5b34996155a3
李泽龙 <wb-lzl443823@alibaba-inc.com>
2019-09-09 16:58:29 +0800

    选择排序

---
+++ sort/src/main/java/com/vrddd/selection/SelectionSort.java
@@ -0,0 +1,37 @@
+package com.vrddd.selection;
+
+import java.util.Arrays;
+
+/**
+ * 选择排序
+ * 让一组数从小到大排列
+ */
+public class SelectionSort {
+    /**
+     * 待排序数组
+     */
+    private static final int[] arr = {32,453,123,43,556,12,312};
+    /**
+     * 执行完后，数组变升序
+     * @param arr 待排序数组
+     */
+    public static void sort(int[] arr){
+        int len = arr.length;
+        for (int i = 0; i < len - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < len; j++) {
+                if(arr[j] < arr[minIndex]){
+                    minIndex = j;
+                }
+            }
+            int mid = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = mid;
+        }
+    }
+
+    public static void main(String[] args) {
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
