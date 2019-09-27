8e99528506644cc773640e2fd603f67dba5125e2
李泽龙 <wb-lzl443823@alibaba-inc.com>
2019-09-27 19:20:18 +0800

    function Object方式

--- introduction/src/main/java/com/vrddd/generic/FindMaxDemo.java
+++ introduction/src/main/java/com/vrddd/generic/FindMaxDemo.java
@@ -18,12 +18,27 @@ public class FindMaxDemo {
         return arr[maxIndex];
     }
 
+    public static <T extends Comparable<T>> T findArrayMax2(T[] arr){
+        int maxIndex = 0;
+        int length = arr.length;
+        for (int i = 1; i < length; i++) {
+            if(arr[i].compareTo(arr[maxIndex]) > 0){
+                maxIndex = i;
+            }
+        }
+        return arr[maxIndex];
+    }
+
     public static void main(String[] args) {
         String[] arr = {"123","321","312"};
         System.out.println(findArrayMax(arr));
 
         Shape[] arr2 = {new Circle(2), new Square(3), new Rectangle(3,4)};
         System.out.println(findArrayMax(arr2));
+
+        System.out.println(findArrayMax2(arr));
+
+        System.out.println(findArrayMax2(arr2));
     }
 
 }
