095ab7a246fcece5c1e7719eda343b479a22e2e7
李泽龙 <wb-lzl443823@alibaba-inc.com>
2019-09-29 20:58:40 +0800

    测试简单有序集合

---
+++ introduction/src/main/java/com/vrddd/introduction/excercise/OrderedCollection.java
@@ -0,0 +1,83 @@
+package com.vrddd.introduction.excercise;
+
+import java.util.Arrays;
+
+public class OrderedCollection <T>{
+
+    private Comparable<T>[] arr;
+    private int size;
+
+    public OrderedCollection(){
+        this(10);
+    }
+
+    public OrderedCollection(int capacity){
+        arr = new Comparable[capacity];
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    public boolean makeEmpty(){
+        try{
+            size = 0;
+            return true;
+        }catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean remove(int i){
+        try{
+            for (int j = i+1; j < size; j++) {
+                arr[j-1] = arr[j];
+            }
+            size--;
+            return true;
+        }catch (Exception ex){
+            ex.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean insert(Comparable ele){
+        try{
+            if(size == arr.length){
+                arr = Arrays.copyOf(arr,size * 2);
+            }
+            int i = 0;
+            for (; i < size; i++) {
+                if (ele.compareTo(arr[i]) < 0){
+                    break;
+                }
+            }
+            for (int j = size; j > i; j--) {
+                arr[j] = arr[j-1];
+            }
+            arr[i] = ele;
+            size++;
+            return true;
+        }catch (Exception ex){
+            ex.printStackTrace();
+            return false;
+        }
+    }
+
+    public Comparable findMin(){
+        if (size > 0){
+            return arr[0];
+        }else{
+            return null;
+        }
+    }
+
+    public Comparable findMax(){
+        if (size > 0){
+            return arr[size-1];
+        }else{
+            return null;
+        }
+    }
+}
--- introduction/src/test/java/com/vrddd/introduction/TestCollection.java
+++ introduction/src/test/java/com/vrddd/introduction/TestCollection.java
@@ -1,6 +1,7 @@
 package com.vrddd.introduction;
 
 import com.vrddd.introduction.excercise.Collection;
+import com.vrddd.introduction.excercise.OrderedCollection;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -29,5 +30,23 @@ public class TestCollection
             collection.insert(i + 120);
         }
         Assert.assertEquals(collection.isPresent(130),true);
+    }
+
+    @Test
+    public void testOrderedCollection()
+    {
+        OrderedCollection collection = new OrderedCollection();
+        Assert.assertEquals(collection.isEmpty(),true);
+        collection.insert(789);
+        Assert.assertEquals(collection.isEmpty(),false);
+        collection.insert(123);
+        collection.remove(0);
+        collection.makeEmpty();
+        Assert.assertEquals(collection.isEmpty(),true);
+        for (int i = 0; i < 11; i++) {
+            collection.insert(120 - i);
+        }
+        System.out.println(collection.findMax());
+        System.out.println(collection.findMin());
     }
 }
