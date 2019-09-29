78dd86c72d456f91d479b179cccfe162ea8998de
李泽龙 <wb-lzl443823@alibaba-inc.com>
2019-09-29 21:11:55 +0800

    测试functionObject

---
+++ introduction/src/main/java/com/vrddd/introduction/excercise/TestFindMax.java
@@ -0,0 +1,39 @@
+package com.vrddd.introduction.excercise;
+
+import com.vrddd.introduction.excercise.entity.Rectangle;
+import com.vrddd.introduction.generic.FindMaxDemo2;
+
+import java.util.Comparator;
+
+public class TestFindMax {
+
+    static Comparator<Rectangle> compArea = (x,y) -> {
+        double b = x.getLength()*x.getWidth() - y.getLength()*y.getWidth();
+        if (b == 0){
+            return 0;
+        }else if (b < 0){
+            return -1;
+        }else{
+            return 1;
+        }
+    };
+
+    static Comparator<Rectangle> compPerimeter = (x,y) -> {
+        double b = x.getLength() + x.getWidth() - y.getLength() - y.getWidth();
+        if (b == 0){
+            return 0;
+        }else if (b < 0){
+            return -1;
+        }else{
+            return 1;
+        }
+    };
+
+    public static void main(String[] args) {
+        Rectangle[] arr = {new Rectangle(12,3),new Rectangle(6,6),new Rectangle(7,8)};
+        Rectangle maxArea = FindMaxDemo2.findMax(arr,compArea);
+        Rectangle maxPerimeter = FindMaxDemo2.findMax(arr,compPerimeter);
+        System.out.println(1);
+    }
+
+}
---
+++ introduction/src/main/java/com/vrddd/introduction/excercise/entity/Rectangle.java
@@ -0,0 +1,21 @@
+package com.vrddd.introduction.excercise.entity;
+
+public class Rectangle {
+
+    private double length;
+    private double width;
+
+    public Rectangle(double length,double width){
+        this.length = length;
+        this.width = width;
+    }
+
+    public double getLength(){
+        return length;
+    }
+
+    public double getWidth(){
+        return width;
+    }
+
+}
