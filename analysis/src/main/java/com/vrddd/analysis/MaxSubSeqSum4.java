12371a7dfa6eb660cf3c3714aa9623d92eef0029
李泽龙 <wb-lzl443823@alibaba-inc.com>
2019-10-08 20:22:38 +0800

    子序列求最大和，线性算法

---
+++ analysis/src/main/java/com/vrddd/analysis/MaxSubSeqSum4.java
@@ -0,0 +1,23 @@
+package com.vrddd.analysis;
+
+public class MaxSubSeqSum4 {
+
+    private static int getSum(int[] arr){
+        int length = arr.length;
+        int maxSum = 0, sum = 0;
+        for (int i = 0; i < length; i++) {
+            sum += arr[i];
+            if (sum < 0){
+                sum = 0;
+            }else if( sum > maxSum){
+                maxSum = sum;
+            }
+        }
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getSum(new int[]{4,-3,5,-2,-1,2,6,-2}));
+    }
+
+}
