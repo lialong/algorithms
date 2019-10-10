c6539c5cfd3c8647640bb49a600b44f2c8969bdb
李泽龙 <wb-lzl443823@alibaba-inc.com>
2019-10-10 09:29:04 +0800

    二分查找，递归、循环解法

--- analysis/src/main/java/com/vrddd/analysis/logarithm/BinarySearch.java
+++ analysis/src/main/java/com/vrddd/analysis/logarithm/BinarySearch.java
@@ -19,9 +19,26 @@ public class BinarySearch {
         }
     }
 
+    private static int search(int[] arr, int num){
+        int start = 0, end = arr.length - 1;
+        while(start <= end){
+            int mid = (start + end) / 2;
+            if (num < arr[mid]){
+                end = mid - 1;
+            }else if(num > arr[mid]){
+                start = mid + 1;
+            }else{
+                return mid;
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5,6,7,8,9,10,11,22,33,44,55,66};
         System.out.println(search(arr, 0, arr.length - 1, 66));
+        System.out.println(search(arr, 66));
+
     }
 
 }
