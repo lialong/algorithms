5172791905ffd3020bf175b2adb269448423aa5e
wb-lzl443823 <wb-lzl443823@autonavi.com>
2019-10-30 16:00:13 +0800

    递归方式求解最大公约数

--- analysis/src/main/java/com/vrddd/analysis/excercise/GcdAlgorithm.java
+++ analysis/src/main/java/com/vrddd/analysis/excercise/GcdAlgorithm.java
@@ -5,14 +5,29 @@ package com.vrddd.analysis.excercise;
 public class GcdAlgorithm {
 
     private static int gcd(int m, int n){
-        while(n > 0){
-            int rem = m % n;
-            m = n;
-            n = rem;
+        if (m > n){
+            if (isEven(m) && isEven(n)){
+                return  2 * gcd( m / 2, n / 2);
+            }else if(isEven(m) && !isEven(n)){
+                return  gcd( m / 2, n);
+            }else if(!isEven(m) && isEven(n)){
+                return  gcd( m, n / 2);
+            }else{
+                return  gcd( (m + n) / 2, (m - n) / 2);
+            }
+        }else if (m == n){
+            return m;
+        }else {
+            return -1;
         }
-        return n;
     }
 
+    private static boolean isEven(int num){
+        return num % 2 == 0;
+    }
 
+    public static void main(String[] args) {
+        System.out.println(gcd(100, 25));
+    }
 
 }
