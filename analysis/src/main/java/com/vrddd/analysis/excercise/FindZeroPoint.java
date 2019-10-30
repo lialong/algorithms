e74ee4f5ff97700f09b80381e2c861d989b3c1bb
wb-lzl443823 <wb-lzl443823@autonavi.com>
2019-10-30 18:37:49 +0800

    二分法找零点

---
+++ analysis/src/main/java/com/vrddd/analysis/excercise/FindZeroPoint.java
@@ -0,0 +1,41 @@
+package com.vrddd.analysis.excercise;
+
+import java.util.function.Function;
+
+public class FindZeroPoint {
+
+    private static Double find(Function<Double,Double> function, double low, double high){
+        double mid = (low + high) / 2;
+        double lowVal = function.apply(low);
+        double highVal = function.apply(mid);
+        double midVal = function.apply(mid);
+        boolean add = lowVal < 0 && high > 0;
+        boolean sub = lowVal > 0 && high < 0;
+        if (add || sub){
+            if(midVal == 0){
+                return mid;
+            }else if(midVal < 0){
+                if (add){
+                    low = mid;
+                }else{
+                    high = mid;
+                }
+            }else if (midVal > 0){
+                if (add){
+                    high = mid;
+                }else{
+                    low = mid;
+                }
+            }
+            return find(function, low, high);
+        }else{
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        Function<Double,Double> function = x -> x - 1;
+        System.out.println(find(function, 0.5, 10));
+    }
+
+}
