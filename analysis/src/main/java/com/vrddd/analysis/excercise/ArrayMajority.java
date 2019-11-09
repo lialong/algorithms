1250e49f7415e1a191e3f23ca6c0f096c659caf3
lialong <437516156@qq.com>
2019-11-09 15:02:51 +0800

    寻找数组主元素

--- analysis/src/main/java/com/vrddd/analysis/excercise/ArrayMajority.java
+++ analysis/src/main/java/com/vrddd/analysis/excercise/ArrayMajority.java
@@ -25,6 +25,25 @@ public class ArrayMajority {
         }
     }
 
+    private static int find(int[] arr,int length){
+        if (length > 0){
+            int length2 = length % 2 == 0 ? length : length - 1;
+            int j = 0;
+            for (int i = 0; i < length2; i += 2) {
+                if(arr[i] == arr[i+1]){
+                    arr[j++] = arr[i];
+                }
+            }
+            int major = find(arr,j);
+            if(length % 2 == 1 && major == -1){
+                major = length - 1;
+            }
+            return isMajority(arr,major) ? major : -1;
+        }else{
+            return -1;
+        }
+    }
+
     private static boolean isMajority(int[] arr,int major){
         if (major > 0){
             int length = arr.length;
@@ -43,9 +62,9 @@ public class ArrayMajority {
     }
 
     public static void main(String[] args) {
-        int[] arr = {3,4,3,4,3,4,3,4,3};
+        int[] arr = {3,3};
         int[] arr2 = new int[arr.length];
-        int major = find(arr,arr2,arr.length);
+        int major = find(arr,arr.length);
         if (major != -1){
             System.out.println(arr[major]);
         }else{
