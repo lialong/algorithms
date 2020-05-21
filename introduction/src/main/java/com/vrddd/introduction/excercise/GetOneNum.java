c04ef7fcc71f58f4992dccac9eaa6a7e9548be4d
along <437516156@qq.com>
2020-05-21 23:09:22 +0800

    计算某数二进制表示时1的个数

--- introduction/src/main/java/com/vrddd/introduction/excercise/GetOneNum.java
+++ introduction/src/main/java/com/vrddd/introduction/excercise/GetOneNum.java
@@ -1,19 +1,26 @@
 package com.vrddd.introduction.excercise;
 
+/**
+ * 练习5：计算某个数的二进制表示中有多少个1
+ * 奇数n中1的个数 = n/2中1的个数 + 1
+ */
 public class GetOneNum {
 
     private static int get(int num){
-        int i = 0;
-        if (num != 0){
-            i = num % 2;
-            i += get(num / 2);
+        if (num == 0){
+            return 0;
+        }else if (num%2 == 0){
+            return get(num + 1) - 1;
+        }else {
+            return get(num/2) + 1;
         }
-        return i;
     }
 
     public static void main(String[] args) {
         int n = 31;
-        System.out.println(get(n / 2));
+        System.out.println(get(n));
+        n = 32;
+        System.out.println(get(n));
     }
 
 }
