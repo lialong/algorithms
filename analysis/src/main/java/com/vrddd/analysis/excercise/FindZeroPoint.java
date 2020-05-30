6e7edff8f5208859bfc4a9e251c7bd4c2e810f3a
along <437516156@qq.com>
2020-05-30 15:43:33 +0800

    msg

--- analysis/src/main/java/com/vrddd/analysis/excercise/FindZeroPoint.java
+++ analysis/src/main/java/com/vrddd/analysis/excercise/FindZeroPoint.java
@@ -2,12 +2,16 @@ package com.vrddd.analysis.excercise;
 
 import java.util.function.Function;
 
+/**
+ * 练习1.18
+ * 找函数零点
+ */
 public class FindZeroPoint {
 
     private static Double find(Function<Double,Double> function, double low, double high){
         double mid = (low + high) / 2;
         double lowVal = function.apply(low);
-        double highVal = function.apply(mid);
+        double highVal = function.apply(high);
         double midVal = function.apply(mid);
         boolean add = lowVal < 0 && highVal > 0;
         boolean sub = lowVal > 0 && highVal < 0;
