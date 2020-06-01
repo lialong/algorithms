5c79e86cf3976307f15ca76c6f8f4985a5f6cef6
李泽龙 <wb-lzl443823@alibaba-inc.com>
2020-06-01 11:28:32 +0800

    栈判断字符串中的平衡符号是否对称

---
+++ list-stack-queue/src/main/java/com/vrddd/basic/stack/TestBalanceSymbol.java
@@ -0,0 +1,57 @@
+package com.vrddd.basic.stack;
+
+import java.util.Stack;
+
+/**
+ * 通过栈来判断字符串中的平衡符号是否是平衡的
+ * @author lizelong
+ * @date 2020/6/1
+ */
+public class TestBalanceSymbol {
+    private static boolean isBalance(String str){
+        Stack<Character> stack = new Stack<>();
+        int length = str.length();
+        for (int i = 0; i < length; i++) {
+            char c = str.charAt(i);
+            if (c == '[' || c == '{' || c == '(' ){
+                stack.push(c);
+            }else if (c == ']'){
+                if (stack.isEmpty()){
+                    return false;
+                }
+                Character character = stack.pop();
+                if (character == null || character != '['){
+                    return false;
+                }
+            }else if (c == '}'){
+                if (stack.isEmpty()){
+                    return false;
+                }
+                Character character = stack.pop();
+                if (character == null || character != '{'){
+                    return false;
+                }
+            }else if (c == ')'){
+                if (stack.isEmpty()){
+                    return false;
+                }
+                Character character = stack.pop();
+                if (character == null || character != '('){
+                    return false;
+                }
+            }
+        }
+        if (stack.isEmpty()){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        String str = "{[()]}";
+        System.out.println(str + " :" + isBalance(str));
+        String str2 = "{[()]}}";
+        System.out.println(str2 + " :" + isBalance(str2));
+    }
+}
